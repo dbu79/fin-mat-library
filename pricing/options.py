f15b3e8af66d3ff1acca7d4d0fe66dfd0993cd1f
Daniel <dbu79@uclive.ac.nz>
2026-06-16 22:27:54 +1200

    Add arithmetic Asian option pricing

--- pricing/options.py
+++ pricing/options.py
@@ -22,6 +22,7 @@ class Option:
         
 
     def price_european(self):
+        # Black-Scholes for European Options
         d1 = (np.log(self.S / self.K) + (self.r + (self.sigma**2)/2)*self.T)/(self.sigma*self.T**0.5)
         d2 = d1 - self.sigma*(self.T**0.5)
 
@@ -36,11 +37,24 @@ class Option:
             print("Valid types are: 'call' and 'put'")
 
 
-    def price_asian(self):
-        d1 = (np.log(self.S / self.K) + (self.r + (self.sigma**2)/2)*self.T)/(self.sigma*self.T**0.5)
-        d2 = d1 - self.sigma*(self.T**0.5)
+    def price_asian(self, n_paths = 10000, n_steps = 252):
+        # Monte Carlo for Arithmetic Asian Options, default 10,000 paths 
+        dt = self.T / n_steps
+        z = np.random.standard_normal((n_paths, n_steps))
+        log_returns = (self.r - 0.5 * self.sigma**2) * dt + self.sigma * np.sqrt(dt) * z 
 
+        log_paths = np.cumsum(log_returns, axis=1)
+        paths = self.S * np.exp(log_paths)
+        avg_prices = paths.mean(axis=1)
         
+        if self.type == 'call':
+            payoffs = np.maximum(avg_prices - self.K, 0)
+            return np.exp(-self.r * self.T) * payoffs.mean()
+        elif self.type == 'put':
+            payoffs = np.maximum(self.K - avg_prices, 0)
+            return np.exp(-self.r * self.T) * payoffs.mean()
+        else:
+            print("Valid types are: 'call' and 'put'")
 
     def price_american(self):
         pass 
