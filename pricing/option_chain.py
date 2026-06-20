8af6aa68b5434e31e5c24d218fad595ccc03984b
Daniel <dbu79@uclive.ac.nz>
2026-06-20 17:18:59 +1200

    adjust default params

--- pricing/option_chain.py
+++ pricing/option_chain.py
@@ -4,22 +4,23 @@ from options import Option
 
 
 class OptionChain:
-    def __init__(self, data, strike_col, price_col):
+    def __init__(self, data, strike_col, price_col, dte_col):
         # Rename inputted df so class methods can work
         self.data = data
         self.data = data.rename(
             columns={
                 strike_col: "strike", 
-                price_col: "market_price", 
+                price_col: "lastPrice", 
                 dte_col: "dte"
             }
         )
+
     def implied_volatilities(self, S, r, opt_type='call'):
         ivs = []
         
         for _, row in self.data.iterrows():
             iv = Option.implied_volatility(
-                market_price=row['market_price'],
+                market_price=row['lastPrice'],
                 S=S,
                 K=row['strike'],
                 T=row['dte']/365,
