df05826be101fa9b39cef2d7d655d5e36403e64f
Daniel <dbu79@uclive.ac.nz>
2026-06-22 11:35:00 +1200

    Bug fix

--- processes/gbm.py
+++ processes/gbm.py
@@ -1,6 +1,4 @@
 import numpy as np
-from scipy.stats import norm
-import matplotlib.pyplot as plt
 
 class GeometricBrownianMotion:
     def sim_path(S, mu, sigma, T, dt, n):
@@ -15,21 +13,8 @@ class GeometricBrownianMotion:
                 time += dt 
 
             if T - time > 0:
-                prices.append(prices[-1]*np.exp((mu - 0.5*(sigma**2))*dt + sigma*np.random.normal(0, np.sqrt(T-time))))
+                prices.append(prices[-1]*np.exp((mu - 0.5*(sigma**2))*(T - time) + sigma*np.random.normal(0, np.sqrt(T-time))))
 
         
             paths.append(prices)
         return paths
-
-
-S = 100 
-mu = 0.08
-sigma = 0.3 
-T = 1
-dt = 1/252
-n = 100
-sample_paths = GeometricBrownianMotion.sim_path(S, mu, sigma, T, dt, n)
-for path in sample_paths:
-    plt.plot(path)
-
-plt.show()
