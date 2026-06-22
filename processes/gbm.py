056d56d9610129c673a626d253f613f2515a3307
Daniel <dbu79@uclive.ac.nz>
2026-06-22 20:17:44 +1200

    Add gbm visualization

--- processes/gbm.py
+++ processes/gbm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt 
 
 class GeometricBrownianMotion:
     def __init__(self, S0, mu, sigma):
@@ -18,4 +19,27 @@ class GeometricBrownianMotion:
                 (self.mu - 0.5*self.sigma**2)*dt + self.sigma*dW[:, i-1]
             )
         return paths
+
+    def show_paths(self, T, dt, n_paths, title='GBM Simulated Paths', xlabel='Price', ylabel='Steps', average=True, largest=True, smallest=True):
+        
+        paths = self.sim_paths(T, dt, n_paths)
+        fig, ax = plt.subplots()
+        for path in paths:
+            ax.plot(path, color='#1f77b4', linewidth=0.75, alpha=0.3)
+        
+        average_ = np.mean(paths, axis=0)
+        largest_ = np.max(paths, axis=0)
+        smallest_ = np.min(paths, axis=0)
+        if average:
+            ax.plot(average_, color='purple', linewidth=0.8, alpha=0.5)
+        if largest:
+            ax.plot(largest_, color='green', linewidth=0.8, alpha=0.3)
+        if smallest:
+            ax.plot(smallest_, color="red", linewidth=0.8, alpha=0.3)
+
+        ax.set_title(f"{title}")
+        ax.set_xlabel(f"{xlabel}")
+        ax.set_ylabel(f"{ylabel}")
+        plt.show()
+
 
