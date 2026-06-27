7565a6ac660a09788c1b5adfc223e07eab86d942
Daniel <dbu79@uclive.ac.nz>
2026-06-28 11:33:55 +1200

    Add arithmetic brownian motion

--- processes/__init__.py
+++ processes/__init__.py
@@ -1,4 +1,5 @@
 from .gbm import GeometricBrownianMotion
+from .abm import ArithmeticBrownianMotion
 from .jump_diffusion import JumpDiffusion
 from .ou import OrnsteinUhlenbeck
 from .vasicek import VasicekModel
---
+++ processes/abm.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ArithmeticBrownianMotion:
+    def __init__(self, S0, mu, sigma):
+        self.S0 = S0
+        self.mu = mu
+        self.sigma = sigma
+    
+    def sim_paths(self, dt, T, n_paths):
+        n_steps = int(T / dt)
+        paths = np.zeros((n_paths, n_steps + 1))
+
+        dW = np.random.normal(0, 1, (n_paths, n_steps))        
+        paths[:, 0] = self.S0
+
+        for i in range(1, n_steps + 1):
+            paths[:, i] = paths[:, i-1] + self.mu*dt + self.sigma*np.sqrt(dt)*dW[:, i - 1]
+
+        return paths
+    
+    def plot_paths(self, paths, title='ABM Simulated Paths', xlabel='Steps', ylabel='Price', average=True, largest=True, smallest=True):
+        fig, ax = plt.subplots()
+        
+        for path in paths:
+            ax.plot(path, color='#1f77b4', linewidth=0.75, alpha=0.5)
+        
+        average_ = np.mean(paths, axis=0)
+        largest_ = np.max(paths, axis=0)
+        smallest_ = np.min(paths, axis=0)
+        if average:
+            ax.plot(average_, color='purple', linewidth=0.8, alpha=0.4)
+        if largest:
+            ax.plot(largest_, color='green', linewidth=0.8, alpha=0.4)
+        if smallest:
+            ax.plot(smallest_, color="red", linewidth=0.8, alpha=0.4)
+
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        plt.show()
+
+
